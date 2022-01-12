fdc74faae2339da93941a1f2a34f5de69d5c63a2
John Tobin <jjtobin@gmail.com>
2022-01-12 15:11:09 -0500

    add script to regenerate weblogs

---
+++ regenerate_weblog.py
@@ -0,0 +1,19 @@
+import numpy as np
+from scipy import stats
+import glob
+execfile('selfcal_helpers.py',globals())
+import pickle
+with open('selfcal_library.pickle', 'rb') as handle:
+    selfcal_library=pickle.load(handle)
+
+with open('solints.pickle', 'rb') as handle:
+    solints=pickle.load(handle)
+
+with open('bands.pickle', 'rb') as handle:
+    bands=pickle.load( handle)
+
+
+generate_weblog(selfcal_library,solints,bands)
+
+
+
