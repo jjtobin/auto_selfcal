d84a5fdd627d09fd37edec75cee11a5df8c4870c
Patrick Sheehan <psheehan287@gmail.com>
2022-12-19 16:48:17 -0500

    Remove need for velocity > 0 at acceleration peaks to catch local minima too.

--- selfcal_helpers.py
+++ selfcal_helpers.py
@@ -2634,9 +2634,7 @@ def unflag_failed_antennas(vis, caltable, flagged_fraction=0.25, only_long_basel
     # case of a significantly flagged short baseline antenna and a lot of minimally flagged long baseline antennas, the velocity
     # might be negative because you have a shallow gap at the intersection of the two. So we need to do a check, and if there's no
     # peaks that satisfy this condition, ignore the velocity criterion.
-    good= np.logical_and(second_derivative[maxima] > 0, derivative[maxima] > 0 )
-    if good.sum() > 0:
-        maxima = maxima[good]
+    maxima = maxima[second_derivative[maxima] > 0]
     # Pick the shortest baseline "significant" maximum.
     good = second_derivative[maxima] / second_derivative[maxima].max() > 0.5
     m = maxima[good].min()
