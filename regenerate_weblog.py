aa93be27beaf844d771da8a611330e209daeea44
Patrick Sheehan <psheehan287@gmail.com>
2023-04-24 07:05:24 -0500

    Revert "Merge the recent Pipeline-related changes"

--- regenerate_weblog.py
+++ regenerate_weblog.py
@@ -1,15 +1,18 @@
-import pickle
 import numpy as np
 from scipy import stats
 import glob
-execfile('selfcal_helpers.py', globals())
+execfile('selfcal_helpers.py',globals())
+import pickle
 with open('selfcal_library.pickle', 'rb') as handle:
-    selfcal_library = pickle.load(handle)
+    selfcal_library=pickle.load(handle)
 
 with open('solints.pickle', 'rb') as handle:
-    solints = pickle.load(handle)
+    solints=pickle.load(handle)
 
 with open('bands.pickle', 'rb') as handle:
-    bands = pickle.load(handle)
+    bands=pickle.load( handle)
 
-generate_weblog(selfcal_library, solints, bands)
+generate_weblog(selfcal_library,solints,bands)
+
+
+
