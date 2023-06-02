f5db0afe4e39ac2d015fc382d2a52bbc26a5a51b
jjtobin <jjtobin@gmail.com>
2023-06-02 10:01:05 -0600

    fix missing argument to get_VLA_bands

--- selfcal_helpers.py
+++ selfcal_helpers.py
@@ -2550,7 +2550,7 @@ def importdata(vislist,all_targets,telescope):
    spwstring=','.join(str(spw) for spw in spwslist)
    nspws_sets=spws_set.shape[0]
    if 'VLA' in telescope:
-      bands,band_properties=get_VLA_bands(vislist)
+      bands,band_properties=get_VLA_bands(vislist,all_targets)
    
    if telescope=='ALMA' or telescope =='ACA':
       bands,band_properties=get_ALMA_bands(vislist,spwstring,spwsarray)
