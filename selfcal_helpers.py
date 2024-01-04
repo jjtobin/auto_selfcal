c8ff3f8521f188a9d19efc54c66893c7c633a194
John Tobin <jjtobin@localhost.localdomain>
2024-01-04 09:08:31 -0500

    sanitize more special characters

--- selfcal_helpers.py
+++ selfcal_helpers.py
@@ -2006,7 +2006,7 @@ def get_uv_range(band,band_properties,vislist):
    return uvrange
 
 def sanitize_string(string):
-   sani_string=string.replace('-','_').replace(' ','_').replace('+','_')
+   sani_string=string.replace('-','_').replace(' ','_').replace('+','_').replace('*','_').replace(',','_').replace(';','_').replace(':','_').replace('[','_').replace(']','_').replace('{','_').replace('}','_')
    sani_string='Target_'+sani_string
    return sani_string
 
