4c016c0ffcebe7ae81f73e7313d9eef8a0dd9303
John Tobin <jjtobin@localhost.localdomain>
2024-01-09 19:14:12 -0500

    fix typo in dict key

--- run_selfcal.py
+++ run_selfcal.py
@@ -377,7 +377,7 @@ def run_selfcal(selfcal_library, selfcal_plan, target, band, telescope, n_ants, 
          if (((post_SNR >= SNR) and (post_SNR_NF >= SNR_NF) and (delta_beamarea < delta_beam_thresh)) or (('inf_EB' in solint) and ((post_SNR-SNR)/SNR > -0.02) and ((post_SNR_NF - SNR_NF)/SNR_NF > -0.02) and (delta_beamarea < delta_beam_thresh))) and np.any(field_by_field_success): 
 
             if do_fallback_combinespw:
-                selfcal_plan[vis]['solint_settings'][solint]['final_model']='combinespw'
+                selfcal_plan[vis]['solint_settings'][solint]['final_mode']='combinespw'
                 if preferred_mode != 'per_spw':
                     for j in range(iteration+1,len(selfcal_plan['solints'])):
                        if 'ap' in selfcal_plan['solints'][j]: # exempt over ap solints since they go back to a longer solint
