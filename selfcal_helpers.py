b0ab08ccabc565ada348415654f2746110946277
John Tobin <jjtobin@localhost.localdomain>
2024-02-26 10:35:04 -0500

    update theoretical noise plot to change alpha depending on noise vs. plot range

--- selfcal_helpers.py
+++ selfcal_helpers.py
@@ -2232,8 +2232,12 @@ def create_noise_histogram_plots(N_1,N_2,intensity_1,intensity_2,rms_1,rms_2,out
    ax.step(intensity_2,N_2/np.max(N_2),label='Final Data')
    ax.plot(intensity_1,gaussian_norm(intensity_1,0,rms_1),label='Initial Gaussian')
    ax.plot(intensity_2,gaussian_norm(intensity_2,0,rms_2),label='Final Gaussian')
+   xplotrange=np.abs(ax.get_xlim()[0])+np.abs(ax.get_xlim()[1])
    if rms_theory !=0.0:
-      ax.fill(intensity_2,gaussian_norm(intensity_2,0,rms_theory),color='gray',label='Theoretical Sensitivity')
+      alpha_plot=-1.0*2.0*rms_theory/xplotrange*0.75 +1.0
+      if (2.0*rms_theory > xplotrange) or (alpha_plot < 0.0):
+         alpha_plot=0.25
+      ax.fill(intensity_2,gaussian_norm(intensity_2,0,rms_theory),color='gray',label='Theoretical Sensitivity',alpha=alpha_plot)
       #ax.plot([-1.0*rms_theory,rms_theory],[0.606,0.606],label='Theoretical Sensitivity')
    ax.legend(fontsize=20)
    ax.set_xlabel('Intensity (mJy/Beam)',fontsize=20)
