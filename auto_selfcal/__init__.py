1001f273f73e07685d78e42f6a61cae9e3d57071
Patrick Sheehan <psheehan287@gmail.com>
2024-06-18 17:30:35 -0400

    Add minimum version via install requirements rather than stopping the code.

--- auto_selfcal/__init__.py
+++ auto_selfcal/__init__.py
@@ -1,20 +1,2 @@
-import casatasks
-import sys
-
-def end_program():
-    print('This version of auto_selfcal requires CASA 6.5.3 or higher to run. Please update your CASA version and try again.')
-    sys.exit(0)
-
-casaversion=casatasks.version()
-if casaversion[0]>=6:
-   if  casaversion[1]>=5:
-      if casaversion[1]==5 and casaversion[2]<3:
-         end_program()
-   else:
-      end_program()
-else:
-   end_program()
-
-
 from .auto_selfcal import auto_selfcal
 from .regenerate_weblog import regenerate_weblog
