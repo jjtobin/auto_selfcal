655c5f294c239630193717ab5920f20eacab9f06
Patrick Sheehan <psheehan287@gmail.com>
2024-11-08 15:59:26 -0600

    Remove a test for now due to space issues.

--- auto_selfcal/tests/test_auto_selfcal.py
+++ auto_selfcal/tests/test_auto_selfcal.py
@@ -1,16 +1,16 @@
 from auto_selfcal import auto_selfcal
 import os
 
-def test_mixed_spw_EBs(tmp_path):
-    d = tmp_path / "mixed_spw_EBs"
-    d.mkdir()
-    os.chdir(d)
-    os.system('wget "https://nrao-my.sharepoint.com/:u:/g/personal/psheehan_nrao_edu/EbUGCua1e95MvPhx3FpfKScBfG5KnxfaIGtk5_JA_Ghw6w?e=OB7Atq&download=1" -O mixed_spw_EBs.tar.gz')
-    os.system('tar xf mixed_spw_EBs.tar.gz')
+#def test_mixed_spw_EBs(tmp_path):
+#    d = tmp_path / "mixed_spw_EBs"
+#    d.mkdir()
+#    os.chdir(d)
+#    os.system('wget "https://nrao-my.sharepoint.com/:u:/g/personal/psheehan_nrao_edu/EbUGCua1e95MvPhx3FpfKScBfG5KnxfaIGtk5_JA_Ghw6w?e=OB7Atq&download=1" -O mixed_spw_EBs.tar.gz')
+#    os.system('tar xf mixed_spw_EBs.tar.gz')
 
-    auto_selfcal()
+#    auto_selfcal()
 
-    assert os.path.exists("weblog")
+#    assert os.path.exists("weblog")
 
 def test_2018_1_01284_S_HOPS_384(tmp_path):
     d = tmp_path / "2018.1.01284.S_HOPS-384"
