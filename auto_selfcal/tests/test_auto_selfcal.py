ca70735c83a4b47412455c6502ea3f879517e8fc
Patrick Sheehan <psheehan287@gmail.com>
2024-11-08 16:30:16 -0500

    Move test data to online location and download from there.

--- auto_selfcal/tests/test_auto_selfcal.py
+++ auto_selfcal/tests/test_auto_selfcal.py
@@ -1,16 +1,14 @@
 from auto_selfcal import auto_selfcal
 import os
-import glob
 
 def test_mixed_spw_EBs(tmp_path):
     d = tmp_path / "mixed_spw_EBs"
     d.mkdir()
     os.chdir(d)
-    os.system("cp -r /lustre/cv/projects/SRDP/selfcal-prototyping/datasets/mixed_spw_EBs/*_targets.ms .")
-    os.system("cp -r /lustre/cv/projects/SRDP/selfcal-prototyping/datasets/mixed_spw_EBs/cont.dat .")
+    os.system('wget "https://nrao-my.sharepoint.com/:u:/g/personal/psheehan_nrao_edu/EbUGCua1e95MvPhx3FpfKScBfG5KnxfaIGtk5_JA_Ghw6w?e=OB7Atq&download=1" -O mixed_spw_EBs.tar.gz')
+    os.system('tar xf mixed_spw_EBs.tar.gz')
 
-    vislist = glob.glob("*_targets.ms")
-    auto_selfcal(vislist)
+    auto_selfcal()
 
     assert os.path.exists("weblog")
 
@@ -18,9 +16,9 @@ def test_2018_1_01284_S_HOPS_384(tmp_path):
     d = tmp_path / "2018.1.01284.S_HOPS-384"
     d.mkdir()
     os.chdir(d)
-    os.system("cp -r /lustre/cv/projects/SRDP/selfcal-prototyping/Patrick/datasets/2018.1.01284.S_HOPS-384/*_targets.ms .")
+    os.system('wget "https://nrao-my.sharepoint.com/:u:/g/personal/psheehan_nrao_edu/ESCyveu27lBBnVHU00RBKuoBAsUZkXXBAZqJyqieCmeFqQ?e=Wpf4ce&download=1" -O 2018.1.01284.S_HOPS-384.tar.gz')
+    os.system('tar xf 2018.1.01284.S_HOPS-384.tar.gz')
 
-    vislist = glob.glob("*_targets.ms")
-    auto_selfcal(vislist)
+    auto_selfcal()
 
     assert os.path.exists("weblog")
