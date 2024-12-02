cd09d9e6a491aa22271d93beef71ac20ce99d96f
Patrick Sheehan <psheehan287@gmail.com>
2024-12-01 23:27:49 -0500

    Update the link to the test data as it now includes the selfcal_library_reference.pickle file.

--- auto_selfcal/tests/test_auto_selfcal.py
+++ auto_selfcal/tests/test_auto_selfcal.py
@@ -63,7 +63,7 @@ def test_2018_1_01284_S_HOPS_384(tmp_path):
     d = tmp_path / "2018.1.01284.S_HOPS-384"
     d.mkdir()
     os.chdir(d)
-    os.system('wget "https://nrao-my.sharepoint.com/:u:/g/personal/psheehan_nrao_edu/ESCyveu27lBBnVHU00RBKuoBAsUZkXXBAZqJyqieCmeFqQ?e=Wpf4ce&download=1" -O 2018.1.01284.S_HOPS-384.tar.gz')
+    os.system('wget "https://nrao-my.sharepoint.com/:u:/g/personal/psheehan_nrao_edu/ESCyveu27lBBnVHU00RBKuoBAsUZkXXBAZqJyqieCmeFqQ?e=Y23i5d&download=1" -O 2018.1.01284.S_HOPS-384.tar.gz')
     os.system('tar xf 2018.1.01284.S_HOPS-384.tar.gz')
 
     auto_selfcal(weblog=False)
