3d856d875ccb187d21ff063deb405647c497cac3
Patrick Sheehan <psheehan287@gmail.com>
2024-12-17 15:36:50 -0600

    Turn all of the tests back on

--- auto_selfcal/tests/test_auto_selfcal.py
+++ auto_selfcal/tests/test_auto_selfcal.py
@@ -63,8 +63,8 @@ def test_benchmark(tmp_path, dataset):
     "zip_file,link",
     [
         pytest.param("2018.1.01284.S_HOPS-384.tar.gz", 'https://nrao-my.sharepoint.com/:u:/g/personal/psheehan_nrao_edu/EavswWlRdDhGrvwJcTPFkrYBTsMmxeazVoy2pwGuHbh8pQ?e=Ncl3hn&download=1', id="2018.1.01284.S_HOPS-384"),
-        #pytest.param("Band8-7m-2.tar.gz", 'https://nrao-my.sharepoint.com/:u:/g/personal/psheehan_nrao_edu/EWld0Re8WDRItLQyctX0kO4B-oYOG4V2k-h4nlhNXJGmcg?e=nSd5id&download=1', id="Band8-7m-2"),
-        #pytest.param("M82-C-conf-C-band_small.tar.gz", 'https://nrao-my.sharepoint.com/:u:/g/personal/psheehan_nrao_edu/EQIHXDI32clBmIT3K4YwIw8BHMEV4FAewqDDr-y4xG5wGw?e=SMVlHy&download=1', id="M82-C-conf-C-band_small"),
+        pytest.param("Band8-7m-2.tar.gz", 'https://nrao-my.sharepoint.com/:u:/g/personal/psheehan_nrao_edu/EWld0Re8WDRItLQyctX0kO4B-oYOG4V2k-h4nlhNXJGmcg?e=nSd5id&download=1', id="Band8-7m-2"),
+        pytest.param("M82-C-conf-C-band_small.tar.gz", 'https://nrao-my.sharepoint.com/:u:/g/personal/psheehan_nrao_edu/EQIHXDI32clBmIT3K4YwIw8BHMEV4FAewqDDr-y4xG5wGw?e=SMVlHy&download=1', id="M82-C-conf-C-band_small"),
     ]
 )
 def test_on_github(tmp_path, request, zip_file, link):
