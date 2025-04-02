6a1b63992f8072959b06dc7689f0a0217b9267c7
Patrick Sheehan <psheehan287@gmail.com>
2025-04-02 07:44:06 -0400

    Check both polarization for flags when dropping solutions from mosaic gaintables

--- gaincal_wrapper.py
+++ gaincal_wrapper.py
@@ -512,7 +512,7 @@ def gaincal_wrapper(selfcal_library, selfcal_plan, target, band, vis, solint, ap
                     if fid_n_solutions < 0.75 * max_n_solutions:
                         flags[:,:,fields == fid] = True
 
-        bad = np.where(flags[0,0,:])[0]
+        bad = np.where(np.any(flags, axis=0)[0])[0]
         tb.removerows(rownrs=bad)
         tb.flush()
         tb.close()
