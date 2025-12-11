853a361a689a68c2fb2ae37bb696624aa4cf0fcb
Patrick Sheehan <psheehan287@gmail.com>
2025-12-11 16:24:42 -0500

    Enable better compatibility for pip-installing into monolithic CASA, including getting working with mpicasa properly, and adding a function to set up the proper symlinks.

--- auto_selfcal/__init__.py
+++ auto_selfcal/__init__.py
@@ -1,3 +1,21 @@
+try:
+    import os
+    if os.getenv("CASAPATH") is not None:
+        import casampi.private.start_mpi
+
+    def setup_monolithic_CASA(pytest=False):
+        casa_path = os.getenv("CASAPATH").split(" ")[0]
+        os.system(f'cd {casa_path}/bin; ln -s casa auto_selfcal')
+
+        if pytest:
+            try:
+                import pytest
+                os.system(f'cd {casa_path}/bin; ln -s casa pytest')
+            except:
+                pass
+except:
+    pass
+
 from .auto_selfcal import auto_selfcal
 from .regenerate_weblog import regenerate_weblog
 from .split_calibrated_final import split_calibrated_final
