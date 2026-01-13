9d093e4677f0495605660742a1294dca12941ee9
jjtobin <jjtobin@gmail.com>
2026-01-13 13:51:47 -0700

    missing bracket

--- auto_selfcal/prepare_selfcal.py
+++ auto_selfcal/prepare_selfcal.py
@@ -63,7 +63,7 @@ def prepare_selfcal(all_targets, bands, bands_for_targets, vislist,
     listdict,bands,band_properties,scantimesdict,scanfieldsdict,scannfieldsdict,scanstartsdict,scanendsdict,integrationsdict,\
     integrationtimesdict,spwslist_dict,spwstring_dict,spwsarray_dict,mosaic_field,gaincalibrator_dict,spectral_scan,spws_set=importdata(vislist,all_targets,bands_for_targets,telescope)
 
-    pprint.pprint(selfcal_library['bands_for_targets')
+    pprint.pprint(selfcal_library['bands_for_targets'])
     ##
     ## Save/restore starting flags
     ##
