3ac55bcfd4f0dc1eaec45c24d42740fc623e24b5
John Tobin <jjtobin@gmail.com>
2026-05-08 11:03:12 -0400

    remove mask check if doing cocal

--- auto_selfcal/run_selfcal.py
+++ auto_selfcal/run_selfcal.py
@@ -57,6 +57,7 @@ def run_selfcal(selfcal_library, selfcal_plan, target, band, n_ants, \
            return
        else:
            for vis in vislist:
+               clearcal(vis=vis,addmodel=True)
                os.system('mv '+vis+' '+vis.replace('.ms','_orig.ms'))
                os.system('mv '+vis+'.flagversions '+vis.replace('.ms','_orig.ms.flagversions'))
                concatvislist=[]
@@ -183,7 +184,7 @@ def run_selfcal(selfcal_library, selfcal_plan, target, band, n_ants, \
 
              # Check that a mask was actually created, because if not the model will be empty and gaincal will do bad things and the 
              # code will break.
-             if not checkmask(sani_target+'_'+band+'_'+solint+'_'+str(iteration)+'.image.tt0'):
+             if not checkmask(sani_target+'_'+band+'_'+solint+'_'+str(iteration)+'.image.tt0') and mode !="cocal":
                  selfcal_library['Stop_Reason'] = 'Empty model for solint '+solint
                  for fid in selfcal_library['sub-fields-to-selfcal']:
                     selfcal_library[fid]['Stop_Reason'] = 'Empty model for solint '+solint
